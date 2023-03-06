9ea04afde4daa0d7f2a9000f4eba2ce2be96cc3a
Stoichiometrical <davidtgondo@gmail.com>
2023-03-06 16:36:57 +0400

    update files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import Home from "./pages/Home";
-import ProductUpload from "./components/productUpload/ProductUpload";
 import Chats from "./pages/Chats";
 import OpenChat from "./components/chat/OpenChat";
 import Login from "./pages/Login";
-import {HashRouter as Router, Switch, Route, Routes, Link, Navigate} from "react-router-dom";
+import {HashRouter as Router, Route, Routes} from "react-router-dom";
 import Register from "./pages/Register";
 import StoreBackend from "./pages/StoreBackend";
 import UploadProduct from "./pages/UploadProduct";
@@ -15,18 +14,15 @@ export default function App(){
             {/*Setting up routes*/}
             <Router>
                 <Routes>
-                    <Route exact  path="/">
                         <Route index element={ <Home/> } />
-                        <Route path = "home" element={ <Home/> } />
-                        <Route path = "login" element={ <Login/> } />
-                        <Route path = "register" element={ <Register/> } />
-                        <Route path="chats" element={ <Chats/> }/>
-                        <Route path="chats/1" element={ <OpenChat/> }/>
-                        <Route path="store" element={ <StoreBackend/> }/>
-                        <Route path="store/upload" element={ <UploadProduct/> }/>
-                    </Route>
-                    <Route exact path="/chat">
-                    </Route>
+                        <Route exact path = "/home" element={ <Home/> } />
+                        <Route exact path = "/login" element={ <Login/> } />
+                        <Route exact path = "/register" element={ <Register/> } />
+                        <Route exact path="/chats" element={ <Chats/> }/>
+                        <Route exact path="/chats/1" element={ <OpenChat/> }/>
+                        <Route exact path="/store" element={ <StoreBackend/> }/>
+                        <Route exact path="/store/upload" element={ <UploadProduct/> }/>
+                        <Route exact path="/chat" />
                 </Routes>
             </Router>
         </>
