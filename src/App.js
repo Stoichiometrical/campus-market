33fb638a17bf8636934ca6903968c0a33eed92a1
Stoichiometrical <davidtgondo@gmail.com>
2023-03-06 16:08:34 +0400

    update package.json

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ProductUpload from "./components/productUpload/ProductUpload";
 import Chats from "./pages/Chats";
 import OpenChat from "./components/chat/OpenChat";
 import Login from "./pages/Login";
-import {BrowserRouter as Router, Switch, Route, Routes, Link, Navigate} from "react-router-dom";
+import {HashRouter as Router, Switch, Route, Routes, Link, Navigate} from "react-router-dom";
 import Register from "./pages/Register";
 import StoreBackend from "./pages/StoreBackend";
 import UploadProduct from "./pages/UploadProduct";
@@ -12,21 +12,21 @@ import UploadProduct from "./pages/UploadProduct";
 export default function App(){
     return(
         <>
-  {/*Setting up routes*/}
+            {/*Setting up routes*/}
             <Router>
                 <Routes>
-                <Route exact  path="/">
-                    <Route index element={ <Home/> } />
-                    <Route path = "home" element={ <Home/> } />
-                    <Route path = "login" element={ <Login/> } />
-                    <Route path = "register" element={ <Register/> } />
-                    <Route path="chats" element={ <Chats/> }/>
-                    <Route path="chats/1" element={ <OpenChat/> }/>
-                    <Route path="store" element={ <StoreBackend/> }/>
-                    <Route path="store/upload" element={ <UploadProduct/> }/>
-                </Route>
-                <Route exact path="/chat">
-                </Route>
+                    <Route exact  path="/">
+                        <Route index element={ <Home/> } />
+                        <Route path = "home" element={ <Home/> } />
+                        <Route path = "login" element={ <Login/> } />
+                        <Route path = "register" element={ <Register/> } />
+                        <Route path="chats" element={ <Chats/> }/>
+                        <Route path="chats/1" element={ <OpenChat/> }/>
+                        <Route path="store" element={ <StoreBackend/> }/>
+                        <Route path="store/upload" element={ <UploadProduct/> }/>
+                    </Route>
+                    <Route exact path="/chat">
+                    </Route>
                 </Routes>
             </Router>
         </>
